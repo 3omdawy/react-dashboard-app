18c38e4d18714190c81a65a082b27b0ab101d552
emadashraf <emad-ashraf@hotmail.com>
2024-08-09 10:05:24 +0300

    Initial commit: page with settings icon and sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { Tooltip } from "@chakra-ui/react";
+import { links } from "../data/dummy";
+
+const Sidebar = () => {
+  const activeMenu = true;
+  const activeLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+  const normalLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 text-md m-2 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray";
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link
+              to="/"
+              onClick={() => {}}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-bold tracking-tight text-slate-900 dark:text-white"
+            >
+              <SiShopware />
+              <span>Shoppy</span>
+            </Link>
+            <Tooltip label="menu" placement="bottom">
+              <button
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+                onClick={() => {}}
+              >
+                <MdOutlineCancel />
+              </button>
+            </Tooltip>
+          </div>
+          <div className="mt-10">
+            {links.map((item) => {
+              return (
+                <div key={item.title}>
+                  <p className="text-gray-400 m-3 mt-4 uppercase">
+                    {item.title}
+                  </p>
+                  {item.links.map((link) => {
+                    return (
+                      <NavLink
+                        to={link.name}
+                        key={link.name}
+                        onClick={() => {}}
+                        className={({ isActive }) =>
+                          isActive ? activeLink : normalLink
+                        }
+                      >
+                        {link.icon}
+                        <span className="capitalize">{link.name}</span>
+                      </NavLink>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Sidebar;
